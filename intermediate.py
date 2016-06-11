62491dced0eac7ce6693f13e34e7a2f7e02022c3
jeankarin <jean.karin@gmail.com>
2016-06-11 04:11:28 +0200

    Se modifican la forma de llamar a las consultas

--- intermediate.py
+++ intermediate.py
@@ -5,13 +5,38 @@
 import con_db
 import cons
 
-def extract_num():
+def extract_num(num):
 	#Create the cursor to database
 	con = con_db.Connect()
 	con.cursor()
 
-	#Input num to search
-	consulta = cons.ConsNum01All()
+	if (num == 1):
+		#Input num to search
+		consulta = cons.ConsNum01All()
+
+	if (num == 2):
+		#Input num to search
+		consulta = cons.ConsNum02All()
+
+	if (num == 3):
+		#Input num to search
+		consulta = cons.ConsNum03All()
+
+	if (num == 4):
+		#Input num to search
+		consulta = cons.ConsNum04All()
+
+	if (num == 5):
+		#Input num to search
+		consulta = cons.ConsNum05All()
+
+	if (num == 6):
+		#Input num to search
+		consulta = cons.ConsStar01All()
+
+	if (num == 7):
+		#Input num to search
+		consulta = cons.ConsStar02All()
 
 	#Execute the query
 	con.executer (consulta)
