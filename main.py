720dee91566cfec4640a81a64ff292529cddeb22
jeankarin <jean.karin@gmail.com>
2016-06-11 06:07:08 +0200

    Creamos fichero main

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#-*-coding:latin-1-*-
+
+# importamos las librerias necesarias
+import intermediate
+
+def main():
+	print ("Posiciones: ")
+	print ("1 Num 1")
+	print ("2 Num 2")
+	print ("3 Num 3")
+	print ("4 Num 4")
+	print ("5 Num 5")
+	print ("6 Star 1")
+	print ("7 Star 2")
+
+	print("Elija la lista de numeros: ")
+	posicion = input()
+
+if __name__ == '__main__':
+	main()
