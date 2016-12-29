82a2d8d5592d181b518d474370a8b7500fe59ec2
jeankarin <jeankarin@gmlocal.es>
2016-12-29 20:54:17 +0100

    Subiendo nuevos archivos

---
+++ insert.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#-*-coding:latin-1-*-
+
+#importamos nuestras librerias
+import connection
+import consultas
+
+def Insert_to_database(num):
+	con = connection.Connect()
+	con.cursor()
+
+	consulta = consultas.InsertNum(num)
+
+	con.executerins(consulta)
+
+	con.close
