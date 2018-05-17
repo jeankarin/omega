18c8426329438a6b7837d24947d9d53a1934b0bb
jeankarin <jeankarin@ES10LP001.greatmaster.local>
2018-05-17 04:00:55 +0200

    Borron

--- README.txt
+++
@@ -1,3 +0,0 @@
-Nom: Omega
-Versión: 1.0
-Fecha: 28/02/2018
--- old_version2/queries.py
+++
@@ -1,7 +0,0 @@
-"""
-Consultas SQL
-"""
-
-def ConsultaFinal():
-	sql = ("""SELECT * FROM NUMEROS ORDER BY ID DESC LIMIT 10""")
-	return sql
