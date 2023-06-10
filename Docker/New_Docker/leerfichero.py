df880c7023bdc92f8f0068e53bb8f13b84a3b9b1
jeankarin <jean.karin@gmail.com>
2023-06-10 18:05:14 +0200

    añadimos los primeros ficheros

--- Docker/New_Docker/leerfichero.py
+++ Docker/New_Docker/leerfichero.py
@@ -6,12 +6,18 @@ import logging
 def LecturaFichero(Lectura):
     numeros = []
 
+    LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+    logging.basicConfig(filename = "omega/Docker/New_Docker/message.log",
+        level = logging.DEBUG,
+        format = LOG_FORMAT)
+    logger = logging.getLogger()
+
     try:
         with open(Lectura,newline='') as csvfile:
             csvreader = csv.reader(csvfile, delimiter=',')
             for row in csvreader:
                 numeros.append(row)
     except FileNotFoundError:
-        print("No existe el fichero numeros.txt")
+        logger.error("Lectura fichero: No existe el fichero numeros.txt ó error leyendo el fichero")
 
     return numeros
---
+++ Docker/New_Docker/main.py
@@ -0,0 +1,8 @@
+## Principal ##
+def main():
+	fichero = leerfichero.LecturaFichero('omega/Docker/New_Docker/numeros.txt')
+	
+
+if __name__ == '__main__':
+	import leerfichero
+	main()
