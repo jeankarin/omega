bce341d7863f5b87fedbb5f28561effa96064822
jeankarin <jean.karin@gmail.com>
2023-11-17 21:09:11 +0100

    Añadimos programa euromillon temporal

---
+++ Docker/Temporal/Linux/app/ficheros_necesarios.py
@@ -0,0 +1,31 @@
+"""
+Funciones para la creación de ficheros y cosas generales necesarias.
+"""
+
+import os
+import sql_connection
+
+# Poblamos el fichero con la información necesaria
+def lastRegistryFile(temp):
+    tempID = 0
+    if (os.stat("/home/jeankarin/Tools/python/euromillon_temporal/files/lastregistry.txt").st_size == 0) or (temp > 0):
+        miConexion = sql_connection.conexionDB()
+        tempID = miConexion.lastregistry()
+
+        # Escribimos los registros en el fichero.
+        with open("/home/jeankarin/Tools/python/euromillon_temporal/files/lastregistry.txt", "w") as file:
+            for i in range(len(tempID)):
+                file.write(str(tempID[i]) + "\n")
+        
+        file.close()
+
+# Creamos los ficheros necesarios.
+def newFiles():
+    temp = 0
+    if (os.path.isfile("/home/jeankarin/Tools/python/euromillon_temporal/files/message.log") == False):
+        os.system("touch /home/jeankarin/Tools/python/euromillon_temporal/files/message.log")
+    if (os.path.isfile("/home/jeankarin/Tools/python/euromillon_temporal/files/lastregistry.txt") == False):
+        os.system("touch /home/jeankarin/Tools/python/euromillon_temporal/files/lastregistry.txt")
+    
+    if (os.path.exists("/home/jeankarin/Tools/python/euromillon_temporal/files/lastregistry.txt")) == True:
+        lastRegistryFile(temp)
